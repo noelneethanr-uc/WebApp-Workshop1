90a9049eaa813abc3b546eb36c981f774319ee8e
mis <noelneethanr@unicourt.com>
2022-08-19 14:27:36 +0530

    demo 1

--- playground/pages/js/index.js
+++ playground/pages/js/index.js
@@ -1,1 +1,19 @@
-console.log("Start ...");
+async function asyncPromise() {
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      res("Done")
+    }, 3000);
+  });
+}
+
+async function main() {
+  console.log("Start . . ");
+
+//   const res = await asyncPromise();
+    asyncPromise().then((data)=> {console.log(data)})
+//   console.log(res);
+
+  console.log("End . . ");
+}
+
+main();
